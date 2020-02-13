c77184aeaa073583850acfa1f5e94c57ccb6594a
electronick3 <nick.iam.more@gmail.com>
2020-02-13 10:06:33 +0300

    some change in struct

--- app/src/main/java/com/example/testgridlayout/model/ApplicationState.kt
+++
@@ -1,7 +0,0 @@
-package com.example.testgridlayout.model
-
-sealed class ApplicationState {
-    object loading: ApplicationState()
-    object error_retry: ApplicationState()
-    object success: ApplicationState()
-}
